514d27fe9feba33ef85ba421b24ca472a2d4a216
Kyoungseo <kyoungseo@hyperithm.com>
2022-04-19 11:44:14 +0900

    Apply eslint and prettier

--- pages/main/[word].tsx
+++ pages/main/[word].tsx
@@ -9,9 +9,9 @@ import { useEffect } from "react";
 
 const cx = classNames.bind(styles);
 
-const axios = require("axios");
+import axios from "axios";
 
-const web3 = require("web3");
+import web3 from "web3";
 
 // type Context = {
 //   params: {
@@ -31,7 +31,7 @@ const web3 = require("web3");
 // };
 
 const fetcher = async (...args: string[]) => {
-  const res = await axios.get(...args);
+  const res = await axios.get(args[0]);
   const longString = (await res.data) as string;
 
   const randomHex = web3.utils.randomHex(32);
@@ -48,10 +48,7 @@ const MainWord: NextPage = () => {
   }, [word]);
 
   // console.log(word);
-  const { data, error } = useSWR(
-    word ? `http://localhost:8000/api/${word}/` : null,
-    fetcher
-  );
+  const { data, error } = useSWR(word ? `http://localhost:8000/api/${word}/` : null, fetcher);
 
   if (error) return <div>Failed to load</div>;
 
@@ -59,22 +56,16 @@ const MainWord: NextPage = () => {
     <div id={styles["main-page"]}>
       <div>
         <div className="title-25 primary-orange">Main page</div>
-        <div className="subtitle-16 black">
-          Page Introduction: this is ted's main page
-        </div>
+        <div className="subtitle-16 black">Page Introduction: this is ted&apos;s main page</div>
         <button type="button" id={styles["wallet-connect"]}>
           Connect Wallet
         </button>
       </div>
       <div>
         <div className={classNames("subtitle-16")}>Web3 random number: </div>
-        <div className={classNames("subtitle-16")}>URL argument: "{word}"</div>
-        <div className={classNames("subtitle-16")}>
-          URL argument - backend response: "{data?.longString}"
-        </div>
-        <div className={classNames("subtitle-16")}>
-          Web3 Random Hex: {data?.randomHex}
-        </div>
+        <div className={classNames("subtitle-16")}>URL argument: &quot;{word}&quot;</div>
+        <div className={classNames("subtitle-16")}>URL argument - backend response: &quot;{data?.longString}&quot;</div>
+        <div className={classNames("subtitle-16")}>Web3 Random Hex: {data?.randomHex}</div>
       </div>
     </div>
   );
--- pages/main/index.tsx
+++ pages/main/index.tsx
@@ -9,19 +9,15 @@ const Main: NextPage = () => {
     <div id={styles["main-page"]}>
       <div>
         <div className="title-25 primary-orange">Main page</div>
-        <div className="subtitle-16 black">
-          Page Introduction: this is ted's main page
-        </div>
+        <div className="subtitle-16 black">Page Introduction: this is ted&apos;s main page</div>
         <button type="button" id={styles["wallet-connect"]}>
           Connect Wallet
         </button>
       </div>
       <div>
         <div className={classNames("subtitle-16")}>Web3 random number: </div>
-        <div className={classNames("subtitle-16")}>URL argument: ''</div>
-        <div className={classNames("subtitle-16")}>
-          URL argument - backend response: ''
-        </div>
+        <div className={classNames("subtitle-16")}>URL argument: &quot;&quot;</div>
+        <div className={classNames("subtitle-16")}>URL argument - backend response: &quot;&quot;</div>
       </div>
     </div>
   );
